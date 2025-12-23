eb88bc927ac2620d5fdc9e984a89e87fc6dc42a0
agnesahyseni <agnesahyseni44@gmail.com>
2025-12-23 22:13:44 +0100

    commit 2
    Some changes and new page added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Explore More button redirect
+const exploreBtn = document.getElementById("ahExploreBtn");
+
+if (exploreBtn) {
+  exploreBtn.addEventListener("click", () => {
+    window.location.href = "places.html";
+  });
+}
